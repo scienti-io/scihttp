46fef78a44a5073239c28fdb9781290f596100b5
André Uchôa <uchoa@scienti.com.br>
2024-02-15 01:04:34 -0300

    feat: make standardHTTPError function public

--- errors.go
+++ errors.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func standardHTTPError(w http.ResponseWriter, sc int) {
+func StandardHTTPError(w http.ResponseWriter, sc int) {
 	msg := fmt.Sprintf("%d %s", sc, http.StatusText(sc))
 	http.Error(w, msg, sc)
 }
 
 func NotImplementedError(w http.ResponseWriter) {
-	standardHTTPError(w, http.StatusNotImplemented)
+	StandardHTTPError(w, http.StatusNotImplemented)
 }
